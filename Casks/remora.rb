e1247dd2061aaf00cd93cf3ef0f1921b3e8d169c
Adil Erchouk <adilrc@icloud.com>
2026-09-04 20:58:48 +0200

    Update remora to 0.3.2

--- Casks/remora.rb
+++ Casks/remora.rb
@@ -1,6 +1,6 @@
 cask "remora" do
-  version "0.3.1"
-  sha256 "c7e42db42f116b50b1f491784966ad03b20b9b8d0edb7c47302816f9e8fdbc5d"
+  version "0.3.2"
+  sha256 "33665b736e28c5280fbc648c350452c717d578e8d477a790ef1ae429f15c091e"
 
   url "https://github.com/adilrc/Remora/releases/download/v#{version}/Remora-#{version}.zip"
   name "Remora"
